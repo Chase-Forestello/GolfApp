f3ffc3be90f25072b791626c98737f1e47faaaa8
Chase Forestello <chase.forestello@gmail.com>
2023-01-12 12:15:09 -0600

    start screen setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 import StartScreen from './components/StartScreen';
 
 function App() {
   return (
     <div className="App">
-      <StartScreen name="Chase" />
+      <StartScreen/>
     </div>
   );
 }
---
+++ src/components/StartScreen.jsx
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import "../styles/StartScreen.css"
+
+export default function StartScreen(props) {
+  const [name, setName] = useState("");
+
+  return (
+    <div className="card start-card">
+      <h1>Start Screen</h1>
+      <p>Enter your name to start the game</p>
+      <input class="start-name-input" name="name" type="text" value={name} placeholder="Player name" onChange={e => setName(e.target.value)} />
+      <button class="btn start-button" onClick={() => props.onStart(name)}>Start</button>
+    </div>
+  );
+}
