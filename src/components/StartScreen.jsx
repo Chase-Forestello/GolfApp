2737eb189a68c2e27950ab66e933ebddef2fa269
Chase Forestello <chase.forestello@gmail.com>
2023-01-12 12:49:30 -0600

    favicon

--- src/components/StartScreen.jsx
+++ src/components/StartScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../styles/StartScreen.css"
+import "../styles/StartScreen.css";
 
 export default function StartScreen(props) {
   const [name, setName] = useState("");
@@ -8,8 +8,20 @@ export default function StartScreen(props) {
     <div className="card start-card">
       <h1>Start Screen</h1>
       <p>Enter your name to start the game</p>
-      <input class="start-name-input" name="name" type="text" value={name} placeholder="Player name" onChange={e => setName(e.target.value)} />
-      <button class="btn start-button" onClick={() => props.onStart(name)}>Start</button>
+      <input
+        class="start-name-input"
+        name="name"
+        type="text"
+        value={name}
+        placeholder="Player name"
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button
+        class="btn btn-primary start-button"
+        onClick={() => props.onStart(name)}
+      >
+        Start
+      </button>
     </div>
   );
 }
