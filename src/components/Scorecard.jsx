54440fff0ead876f56ac4ee6341948789e76813d
Chase Forestello <chase.forestello@gmail.com>
2023-02-21 20:23:34 -0600

    modified scorecard for simpler inputs. Issues with specific player targeting

--- src/components/Scorecard.jsx
+++ src/components/Scorecard.jsx
@@ -3,6 +3,7 @@ import { useLocation } from "react-router-dom";
 import "../styles/playerNames.css";
 import "../styles/scorecard.css";
 import map from "../img/theQuarryGolfCourse.png";
+import { Dropdown } from "bootstrap";
 
 export default function Scorecard(props) {
   const { state } = useLocation();
@@ -21,67 +22,75 @@ export default function Scorecard(props) {
   const inputForm = (
     <div>
       <input
-        id="ticketNum"
+        id="strokeNum"
         type="number"
-        name="ticketNum"
-        list="defaultNumbers"
+        inputMode="numeric"
+        pattern="[0-9]*"
+        name="strokeNum"
         className="form-control text-center score-input"
       />
-      <datalist id="defaultNumbers">
-        <option value="00">00</option>
-        <option value="-3">-3</option>
-        <option value="-2">-2</option>
-        <option value="-1">-1</option>
-        <option value="0">0</option>
-        <option value="1">+1</option>
-        <option value="2">+2</option>
-        <option value="3">+3</option>
-        <option value="4">+4</option>
-      </datalist>
     </div>
   );
-  function handleYardageChange(e) {
-    let yardage = e.target.value;
-    let yardages = "";
-    let player = e.target.id;
-    console.log(player.charAt(player.length - 1));
-    console.log(yardage);
-    let test = document.getElementsByClassName(
-      "yardage" + player.charAt(player.length - 1)
-    );
-    console.log(test);
-    for (let i = 0; i < test.length; i++) {
-      console.log(test[i].innerHTML);
-    }
 
-    if (yardage === "regular") {
-      yardages = [
-        330, 385, 106, 258, 474, 293, 360, 125, 267, 413, 318, 138, 292, 381,
-        480, 174, 309, 473,
-      ];
-    } else if (yardage === "championship") {
-      yardages = [
-        361, 423, 136, 292, 514, 324, 374, 142, 307, 437, 351, 164, 331, 410,
-        495, 201, 657, 509,
-      ];
-    } else if (yardage === "professional") {
-      yardages = [
-        388, 453, 168, 325, 544, 383, 399, 158, 349, 474, 370, 205, 362, 442,
-        528, 242, 386, 564,
-      ];
-    }
-    for (let i = 0; i < test.length; i++) {
-      test[i].innerHTML = yardages[i];
-    }
-  }
+  
+  const parForm = (
+    <div>
+      <select className="par-inputs">
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+    </div>
+  );
+
+  // function handleYardageChange(e) {
+  //   let yardage = e.target.value;
+  //   let yardages = "";
+  //   let player = e.target.id;
+  //   console.log(player.charAt(player.length - 1));
+  //   console.log(yardage);
+  //   let test = document.getElementsByClassName(
+  //     "yardage" + player.charAt(player.length - 1)
+  //   );
+  //   console.log(test);
+  //   for (let i = 0; i < test.length; i++) {
+  //     console.log(test[i].innerHTML);
+  //   }
+
+  //   if (yardage === "regular") {
+  //     yardages = [
+  //       330, 385, 106, 258, 474, 293, 360, 125, 267, 413, 318, 138, 292, 381,
+  //       480, 174, 309, 473,
+  //     ];
+  //   } else if (yardage === "championship") {
+  //     yardages = [
+  //       361, 423, 136, 292, 514, 324, 374, 142, 307, 437, 351, 164, 331, 410,
+  //       495, 201, 657, 509,
+  //     ];
+  //   } else if (yardage === "professional") {
+  //     yardages = [
+  //       388, 453, 168, 325, 544, 383, 399, 158, 349, 474, 370, 205, 362, 442,
+  //       528, 242, 386, 564,
+  //     ];
+  //   }
+  //   for (let i = 0; i < test.length; i++) {
+  //     test[i].innerHTML = yardages[i];
+  //   }
+  // }
 
   function handleTotalClick(e) {
     let total = 0;
     let player = e.target.id;
+    let parTotal = [];
+    let parInputs = document.getElementsByClassName("par-inputs");
+    for(let i = 0; i < parInputs.length; i++){
+      console.log(parInputs[i].value);
+      parTotal.push(parInputs[i].value);
+    }
+    console.log(parTotal);
     console.log(player);
-    let scoreInputs = document.getElementsByClassName(
-      "score-input" + player.charAt(player.length - 1)
-    );
+    let scoreInputs = document.getElementsByClassName("score-input");
+    console.log(scoreInputs.innerHTML);
     player = "total" + player.charAt(player.length - 1);
     for (let i = 0; i < scoreInputs.length; i++) {
       if (scoreInputs[i].value !== "") {
@@ -131,7 +140,7 @@ export default function Scorecard(props) {
                       <thead>
                         <tr>
                           <th scope="col">Hole</th>
-                          <th scope="col">
+                          {/* <th scope="col">
                             <div>
                               <select
                                 onChange={handleYardageChange}
@@ -148,7 +157,7 @@ export default function Scorecard(props) {
                               </select>
                             </div>
                             Yardage
-                          </th>
+                          </th> */}
                           <th scope="col">Par</th>
                           <th scope="col">Score</th>
                         </tr>
@@ -156,110 +165,110 @@ export default function Scorecard(props) {
                       <tbody>
                         <tr>
                           <th scope="row">1</th>
-                          <td className={"yardage" + i}>330</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>330</td> */}
+                          <td>{parForm}</td>
                           <td className="score-input-div">{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">2</th>
-                          <td className={"yardage" + i}>385</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>385</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">3</th>
-                          <td className={"yardage" + i}>106</td>
-                          <td>3</td>
+                          {/* <td className={"yardage" + i}>106</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">4</th>
-                          <td className={"yardage" + i}>258</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>258</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">5</th>
-                          <td className={"yardage" + i}>474</td>
-                          <td>5</td>
+                          {/* <td className={"yardage" + i}>474</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">6</th>
-                          <td className={"yardage" + i}>293</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>293</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">7</th>
-                          <td className={"yardage" + i}>360</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>360</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">8</th>
-                          <td className={"yardage" + i}>125</td>
-                          <td>3</td>
+                          {/* <td className={"yardage" + i}>125</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">9</th>
-                          <td className={"yardage" + i}>267</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>267</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">10</th>
-                          <td className={"yardage" + i}>413</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>413</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">11</th>
-                          <td className={"yardage" + i}>318</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>318</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">12</th>
-                          <td className={"yardage" + i}>138</td>
-                          <td>3</td>
+                          {/* <td className={"yardage" + i}>138</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">13</th>
-                          <td className={"yardage" + i}>292</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>292</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">14</th>
-                          <td className={"yardage" + i}>381</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>381</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">15</th>
-                          <td className={"yardage" + i}>480</td>
-                          <td>5</td>
+                          {/* <td className={"yardage" + i}>480</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">16</th>
-                          <td className={"yardage" + i}>174</td>
-                          <td>3</td>
+                          {/* <td className={"yardage" + i}>174</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">17</th>
-                          <td className={"yardage" + i}>309</td>
-                          <td>4</td>
+                          {/* <td className={"yardage" + i}>309</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                         <tr>
                           <th scope="row">18</th>
-                          <td className={"yardage" + i}>473</td>
-                          <td>5</td>
+                          {/* <td className={"yardage" + i}>473</td> */}
+                          <td>{parForm}</td>
                           <td>{inputForm}</td>
                         </tr>
                       </tbody>
@@ -268,7 +277,6 @@ export default function Scorecard(props) {
                       type="button"
                       id={"total" + i}
                       onClick={handleTotalClick}
-                      disabled
                       className="btn btn-primary"
                     >
                       Total
@@ -300,15 +308,15 @@ export default function Scorecard(props) {
         <div className="col-sm-12 col-12">
           <div className="card" id="scoreCard">
             <div id="scoreCardHeader" className="card">
-              <h1 id="courseNameText">The Quarry Golf Course</h1>
-              <button
+              <h1 id="courseNameText">Scorecard</h1>
+              {/* <button
                 type="button"
                 className="btn map-btn btn-primary"
                 data-bs-toggle="modal"
                 data-bs-target="#exampleModal"
               >
                 Map
-              </button>
+              </button> */}
 
               <div
                 className="modal fade"
@@ -331,7 +339,7 @@ export default function Scorecard(props) {
                         aria-label="Close"
                       ></button>
                     </div>
-                    <div className="modal-body map-body">
+                    {/* <div className="modal-body map-body">
                       <img
                         id="quarryMap"
                         src={map}
@@ -339,7 +347,7 @@ export default function Scorecard(props) {
                         alt="map"
                         className="map-img"
                       />
-                    </div>
+                    </div> */}
                     <div className="modal-footer map-footer">
                       <button
                         type="button"
